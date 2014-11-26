78ce8b0466821cb8f16748bfd3c0a04d1e5a3529
Calvin Kern <calvinmkern@gmail.com>
2014-11-26 12:23:14 -0700

    Initial commit

---
+++ Ray Tracer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Ray Tracer
+//
+//  Created by Calvin Kern on 11/26/14.
+//  Copyright (c) 2014 Calvin Kern. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
